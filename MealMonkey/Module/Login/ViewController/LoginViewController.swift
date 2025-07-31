519cd6250f25fba6f2b7f993a13397485edc4c4b
divyeshrupapara <>
2025-07-31 17:26:54 +0530

    Add new App, Task Splash screen done.

---
+++ MealMonkey/Module/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MealMonkey
+//
+//  Created by Divyesh Rupapara on 31/07/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Log in"
+        self.navigationController?.isNavigationBarHidden = false
+    }
+}
+
