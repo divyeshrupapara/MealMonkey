97ac870202d14f2be9a63ecfe64392279b0baff2
divyeshrupapara <>
2025-08-01 20:10:36 +0530

    Task create Forget Passsword page done.

--- MealMonkey/Module/ForgetPassword/ViewController/ForgetPasswordViewController.swift
+++ MealMonkey/Module/ForgetPassword/ViewController/ForgetPasswordViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ForgetPasswordViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var btnSend: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Forget Password"
-        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.isNavigationBarHidden = true
         
+        viewStyle(textfield: [txtEmail, btnSend])
+        
+        setPadding(textfield: [txtEmail])
     }
+    
+    func viewStyle(textfield: [UIView]){
+        for item in textfield {
+            item.viewStyle(cornerRadius: 28, borderWidth: 0, borderColor: .systemGray)
+        }
+    }
+    
+    func setPadding(textfield: [UITextField]){
+        for item in textfield {
+            item.setPadding(left: 34, right: 34)
+        }
+    }
+    
 }
